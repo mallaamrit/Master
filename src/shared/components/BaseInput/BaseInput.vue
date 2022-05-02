bd83e9f3783184c25110476d5f1b784d6b9a5e85
papish.limbu <papish.limbu@omnicommediagroup.com>
2022-05-02 06:54:31 +0545

    wip: base components docs

--- src/shared/components/BaseInput/BaseInput.vue
+++ src/shared/components/BaseInput/BaseInput.vue
@@ -12,8 +12,8 @@
       :class="computeClasses"
     >
       <slot name="icon">
-        <span class="mx-2">
-          <BaseIcon v-if="icon" :name="icon"></BaseIcon>
+        <span class="mr-2" v-if="icon">
+          <BaseIcon :name="icon"></BaseIcon>
         </span>
       </slot>
       <input
@@ -28,8 +28,26 @@
         v-bind="$attrs"
       />
       <slot name="clearable">
-        <span v-if="clearable" class="max-w-[100px]">
-          <BaseIcon name="x-circle" @click="onClear"></BaseIcon>
+        <span
+          v-if="clearable"
+          class="max-w-[100px] cursor-pointer"
+          @click="onClear"
+          @keydown="onClear"
+        >
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20Z"
+              fill="#9EB3CC"
+            />
+            <path d="M7 7L13 13" stroke="white" />
+            <path d="M13 7L7 13" stroke="white" />
+          </svg>
         </span>
       </slot>
     </div>
@@ -82,7 +100,7 @@ export default {
       default: false,
     },
   },
-  emits: ["update:modelValue"],
+  emits: ["update:modelValue", "clear"],
   setup(props, context) {
     const uniqueNameIdentifier = computed(() => props.name.replace(/\s/g, ""));
 
@@ -106,6 +124,7 @@ export default {
 
     function onClear() {
       context.emit("update:modelValue", " ");
+      context.emit("clear");
     }
 
     return {
--- src/views/DashboardIndex.vue
+++ src/views/DashboardIndex.vue
@@ -1,40 +1,66 @@
 <template>
-  <div class="p-10">
-    <h1>Component/Docs</h1>
+  <div class="p-20 bg-[#FAFAFA] min-h-screen">
+    <div class="bg-[#faf0f0] p-10 border-2 border-gray-100 rounded shadow-sm">
+      <h1 class="font-semibold text-2xl">Component/Docs</h1>
 
-    <h2>Button</h2>
-    <BaseButton class="mr-5" color="primary" @click="onClick">
-      Click Me!</BaseButton
-    >
-    <BaseButton color="light">Click Me!</BaseButton>
+      <div class="my-16">
+        <h2 class="font-medium text-lg mb-2">Base Button</h2>
+        <BaseButton class="mr-5" color="primary" @click="onClick">
+          Click Me!</BaseButton
+        >
+        <BaseButton color="light">Click Me!</BaseButton>
+      </div>
 
-    <div class="my-10">
-      <BaseInput
-        disabled
-        type="text"
-        v-model="text"
-        class="custom-input"
-        :success="false"
-      ></BaseInput>
-      {{ text }}
+      <div class="my-16">
+        <h2 class="font-medium text-lg mb-2">Base Input</h2>
+        <BaseInput type="text" v-model="text"></BaseInput>
+        <div>Input value: {{ text }}</div>
+      </div>
+
+      <div class="my-16">
+        <h2 class="font-medium text-lg mb-2">Base Select</h2>
+        <BaseSelect
+          v-model="selectedOption"
+          :options="selectOptions"
+          label="Country"
+          option-text="text"
+        ></BaseSelect>
+        <div>
+          Selected country:
+          <span v-if="selectedOption !== ''">{{
+            selectOptions.find((opt) => opt.id === selectedOption).text
+          }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
-import BaseButton from "@/shared/components/BaseButton/BaseButton.vue";
 
 export default {
-  components: { BaseButton },
   setup() {
     const text = ref("");
+    const selectOptions = ref([
+      {
+        id: 1,
+        text: "Australia",
+        value: "aus",
+      },
+      {
+        id: 2,
+        text: "Canada",
+        value: "ca",
+      },
+    ]);
+    const selectedOption = ref("");
 
     function onClick() {
-      console.log(1);
+      console.log("clicked....");
     }
 
-    return { text, onClick };
+    return { text, selectOptions, selectedOption, onClick };
   },
 };
 </script>
