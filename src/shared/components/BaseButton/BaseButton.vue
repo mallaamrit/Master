443a3d784d272d015046421ba0c99d312a01bd51
Papish Limbu <papish.limbu@omnicommediagroup.com>
2022-09-12 15:57:36 +0545

    wip: testing new component structure

--- src/shared/components/BaseButton/BaseButton.vue
+++ src/shared/components/BaseButton/BaseButton.vue
@@ -1,19 +1,31 @@
 <template>
-  <button
-    :type="type"
-    :disabled="disabled"
-    class="inline-block rounded-md border transition duration-100 ease-in-out active:outline active:outline-1 active:outline-blue-400 overflow-hidden text-sm font-semibold"
-    :class="classes"
-    @click="handleClickEvent"
-    v-bind="$attrs"
-  >
+  <component :is="element">
     <slot />
-  </button>
+  </component>
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, h } from "vue";
+import { RouterLink } from "vue-router";
 import { BUTTON_COLORS, BUTTON_TYPES, BUTTON_SIZES } from "./types";
+
+const BUTTON_CONFIG = {
+  default:
+    "inline-block text-center leading-6 align-middle	 rounded-md border transition duration-100 ease-in-out active:outline active:outline-1 active:outline-blue-400 overflow-hidden  font-semibold",
+  color: {
+    primary: "bg-blue-500 border-blue-500 text-white",
+    secondary: "bg-blue-400 border-blue-400 text-white",
+    success: "bg-blue-300 border-blue-300 text-black",
+    info: "bg-blue-200 border-blue-200 text-black",
+    light: "bg-white border-blue-500 text-black",
+  },
+  size: {
+    small: "px-4 py-1 text-xs",
+    normal: "px-6 py-2 text-sm",
+    large: "px-7 py-4 text-lg",
+  },
+  disabled: "select-none pointer-events-none",
+};
 
 export default {
   name: "BaseButton",
@@ -44,59 +56,76 @@ export default {
       type: Boolean,
       default: false,
     },
+    href: {
+      type: String,
+      default: "",
+    },
+    to: {
+      type: Object || String,
+      default(val) {
+        if (typeof val === "object") {
+          return {};
+        }
+        return "";
+      },
+    },
   },
   emits: ["click"],
-  setup(props, context) {
-    const classes = computed(() => ({
-      [`button-${props.color}`]: !!props.color,
-      [`button-${props.size}`]: !!props.size,
-      [`pointer-events-none select-none`]: props.disabled,
-    }));
+  setup(props, ctx) {
+    const isDisabled = computed(() =>
+      props.disabled ? BUTTON_CONFIG.disabled : ""
+    );
 
-    function handleClickEvent(event) {
+    const classes = computed(() => [
+      BUTTON_CONFIG.default,
+      BUTTON_CONFIG.color[props.color],
+      BUTTON_CONFIG.size[props.size],
+      isDisabled.value,
+    ]);
+
+    function onClick(event) {
       if (props.disabled) {
         event.preventDefault();
         event.stopPropagation();
       } else {
-        context.emit("click");
+        ctx.emit("click", event);
       }
     }
 
-    return { classes, handleClickEvent };
+    const events = {
+      onClick,
+    };
+
+    const isButton = props.type.toLowerCase() !== "link";
+
+    function configure() {
+      const config = {
+        class: classes.value,
+      };
+      if (props.disabled === false && props.to) {
+        config.to = props.to;
+      }
+      return config;
+    }
+
+    const linkComponent = () =>
+      props.disabled === false
+        ? h(RouterLink, { ...configure() }, ctx.slots.default)
+        : h("a", { ...configure() }, ctx.slots.default);
+
+    const buttonComponent = () =>
+      h("button", { class: classes.value, ...events }, ctx.slots.default);
+
+    // const componentData = {
+    //   class: '',
+    //   props: '',
+    //   attrs: '',
+    //   events: '',
+    // }
+
+    const element = isButton ? buttonComponent() : linkComponent();
+
+    return { element };
   },
 };
 </script>
-
-<style scoped>
-.button-primary {
-  @apply bg-blue-500 border-blue-500 text-white;
-}
-
-.button-secondary {
-  @apply bg-blue-400 border-blue-400 text-white;
-}
-
-.button-success {
-  @apply bg-blue-300 border-blue-300 text-black;
-}
-
-.button-info {
-  @apply bg-blue-200 border-blue-200 text-black;
-}
-
-.button-light {
-  @apply bg-white border-blue-500 text-black;
-}
-
-.button-normal {
-  @apply px-6 py-[13px];
-}
-
-.button-small {
-  @apply px-4 py-[5px];
-}
-
-.button-large {
-  @apply px-7 py-4;
-}
-</style>
